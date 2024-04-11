648f4891953d8a7a50264108e15f43029b86e17d
Jorge Francisco Aguirre Concha <jorgef.aguir@gmail.com>
2024-04-11 12:46:54 +1200

    deleted some icons a move nav to his own component

---
+++ src/components/MainNav.svelte
@@ -0,0 +1,34 @@
+<script>
+    import {AppBar} from '@skeletonlabs/skeleton';
+</script>
+<AppBar>
+    <svelte:fragment slot="lead">
+        <strong class="text-xl uppercase">Fck Gravity</strong>
+    </svelte:fragment>
+    <svelte:fragment slot="trail">
+        <a
+            class="btn btn-sm variant-ghost-surface"
+            href="https://discord.gg/EXqV7W8MtY"
+            target="_blank"
+            rel="noreferrer"
+        >
+            Sing In
+        </a>
+        <a
+            class="btn btn-sm variant-ghost-surface"
+            href="https://twitter.com/SkeletonUI"
+            target="_blank"
+            rel="noreferrer"
+        >
+            Sing Up
+        </a>
+        <a
+            class="btn btn-sm variant-ghost-surface"
+            href="https://github.com/skeletonlabs/skeleton"
+            target="_blank"
+            rel="noreferrer"
+        >
+            Sing Out
+        </a>
+    </svelte:fragment>
+</AppBar>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,43 +1,13 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { AppShell } from '@skeletonlabs/skeleton';
+	import MainNav from '../components/MainNav.svelte';
 </script>
 
 <!-- App Shell -->
 <AppShell>
 	<svelte:fragment slot="header">
-		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Atre's Utils</strong>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					link1
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					link2
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					link3
-				</a>
-			</svelte:fragment>
-		</AppBar>
+		<MainNav/>
 	</svelte:fragment>
 	<!-- Page Route Content -->
 	<slot />
